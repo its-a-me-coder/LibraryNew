1192a312c03e6f94e7972f84bef286a2632c2de8
Sarthak Khurana <sarthak.khurana@hanu.com>
2022-11-25 12:45:04 +0530

    changes done

--- LibraryMainApp/Books/BooksSQL/book_connection.py
+++ LibraryMainApp/Books/BooksSQL/book_connection.py
@@ -9,3 +9,19 @@ c = conn.cursor()
 c.execute("""SELECT book_name FROM book
 """)
 print(c.fetchall())
+
+#getByGenre
+def getByGenre():
+    gen = 'Fiction'
+    book_genre=c.execute("SELECT Book_Name,Author_Name,Edition FROM Book WHERE Genre=?",(gen,))
+    print(book_genre.fetchall())
+
+#deleteBook
+def deleteBook():
+    del_id=1
+    c.execute("DELETE FROM Book WHERE Book_id =?",(del_id,))
+    after_del=c.execute("SELECT * FROM Book")
+    print(after_del.fetchall())
+
+getByGenre()
+deleteBook()
