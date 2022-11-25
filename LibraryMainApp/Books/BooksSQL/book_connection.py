5ab25342d10308c56254a42f38226e9aaf212b3e
Siddharth Rai <siddharth.rai@hanu.com>
2022-11-25 12:37:21 +0530

    adding get book availblity function

--- LibraryMainApp/Books/BooksSQL/book_connection.py
+++ LibraryMainApp/Books/BooksSQL/book_connection.py
@@ -8,8 +8,12 @@ c = conn.cursor()
 
 # get avalibility of the books
 def getavailbility():
-    ava = c.execute("SELECT Book_Name,Available_Copies FROM book")
-    print(ava.fetchall())
+    c.execute("SELECT Book_Name,Available_Copies FROM book")
+    rec = c.fetchall()
+    ava = [x[1] for x in rec]
+    book = [x[0] for x in rec]
+    for i in range(len(book)-1):
+        print(f" {book[i]}, availability : {ava[i]}")
 
 
 getavailbility()
