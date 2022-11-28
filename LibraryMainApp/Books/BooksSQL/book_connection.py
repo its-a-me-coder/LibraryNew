0efb442e719e0f0d480e48fbd0250fdb18254a0d
SHRESTH.GUPTA@hanuglobal.hanu.com <SHRESTH.GUPTA@hanuglobal.hanu.com>
2022-11-28 10:41:39 +0530

    Get by author and update column

--- LibraryMainApp/Books/BooksSQL/book_connection.py
+++ LibraryMainApp/Books/BooksSQL/book_connection.py
@@ -1,11 +1,24 @@
 import sqlite3
 
-# connect to database for good purpose
+# connect to database
 conn = sqlite3.connect("../../../library.db")
 
 # create a cursor/ pointer to the database
 c = conn.cursor()
 
-c.execute("""SELECT book_name FROM book
-""")
-print(c.fetchall())
+# get avalibility of the books
+def getByAuthor():
+    Aut = 'Britt Bennett'
+    book_author=c.execute("SELECT Book_Name,Author_Name,Edition FROM Book WHERE Author_Name=?",(Aut,))
+    print(book_author.fetchall())
+
+
+getByAuthor()
+
+def Update_Column():
+    update_column=c.execute("ALTER TABLE Book ADD COLUMN phone VARCHAR(50)")
+    book=c.execute("SELECT * FROM Book")
+    print(book.fetchall())
+
+
+Update_Column()
