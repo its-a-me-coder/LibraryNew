dde6b0bd31c18bccd79e76d323e659e6c60e23c9
Sourik <dutta.sourik@gmail.com>
2022-11-28 11:13:21 +0530

    added functions

--- LibraryMainApp/Books/BooksSQL/book_connection.py
+++ LibraryMainApp/Books/BooksSQL/book_connection.py
@@ -6,6 +6,13 @@ conn = sqlite3.connect("../../../library.db")
 # create a cursor/ pointer to the database
 c = conn.cursor()
 
-c.execute("""SELECT book_name FROM book
-""")
-print(c.fetchall())
+
+# getByGenre
+def get_book_names():
+    book_names = c.execute("SELECT Book_Name,Author_Name,Edition,Genre FROM Book ")
+    return tuple(book_names)
+
+
+def update_total_quantity(bookid, total):
+    c.execute('''UPDATE Book SET Total_Copies = ?  WHERE  Book_ID = ?''', (total, bookid))
+    conn.commit()
