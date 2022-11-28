f37c34da810d405565cd26cfc9728f72df299520
Sarthak Khurana <sarthak.khurana@hanu.com>
2022-11-28 12:28:37 +0530

    adding return statement

--- LibraryMainApp/Books/BooksSQL/book_connection.py
+++ LibraryMainApp/Books/BooksSQL/book_connection.py
@@ -6,22 +6,16 @@ conn = sqlite3.connect("../../../library.db")
 # create a cursor/ pointer to the database
 c = conn.cursor()
 
-c.execute("""SELECT book_name FROM book
-""")
-print(c.fetchall())
+
 
 #getByGenre
-def getByGenre():
-    gen = 'Fiction'
+def getByGenre(gen):
     book_genre=c.execute("SELECT Book_Name,Author_Name,Edition FROM Book WHERE Genre=?",(gen,))
-    print(book_genre.fetchall())
+    return tuple(book_genre)
 
 #deleteBook
-def deleteBook():
-    del_id=1
+def deleteBook(del_id):
     c.execute("DELETE FROM Book WHERE Book_id =?",(del_id,))
-    after_del=c.execute("SELECT * FROM Book")
-    print(after_del.fetchall())
 
-getByGenre()
-deleteBook()
+
+
