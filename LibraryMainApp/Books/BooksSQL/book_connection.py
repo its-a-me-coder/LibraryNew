b2fe603051c1d765f3a0a185291fcdd0080ec47b
SHRESTH.GUPTA@hanuglobal.hanu.com <SHRESTH.GUPTA@hanuglobal.hanu.com>
2022-11-28 13:00:48 +0530

    Added changes

--- LibraryMainApp/Books/BooksSQL/book_connection.py
+++ LibraryMainApp/Books/BooksSQL/book_connection.py
@@ -7,17 +7,16 @@ conn = sqlite3.connect("../../../library.db")
 c = conn.cursor()
 
 # get avalibility of the books
-def getByAuthor():
-    Aut = 'Britt Bennett'
+def getByAuthor(Aut):
     book_author=c.execute("SELECT Book_Name,Author_Name,Edition FROM Book WHERE Author_Name=?",(Aut,))
     return tuple(book_author.fetchall())
 
 
 
 
-def Update_Column():
-    update_column=c.execute("ALTER TABLE Book ADD COLUMN phone VARCHAR(50)")
-    book=c.execute("SELECT * FROM Book")
-    return(book.fetchall())
+def Update_Column(Root_ID, id,Name, Total, Available, genre, Aname, Ed):
+    c.execute('''UPDATE Book SET Book_ID =?,Book_Name =?, Total_Copies=?, Available_Copies = ?,Genre=?,Author_Name=?,Edition=?  WHERE  Book_ID = ?''',(id,Name, Total, Available, genre, Aname, Ed, Root_ID))
+    conn.commit()
+
 
 
