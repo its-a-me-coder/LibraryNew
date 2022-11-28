85b19b91d55a7415a8c232e7fc530b395a3fb1ad
Sarthak Khurana <sarthak.khurana@hanu.com>
2022-11-28 16:35:32 +0530

    adding history from lending

--- LibraryMainApp/User/UsersSQL/user_connection.py
+++ LibraryMainApp/User/UsersSQL/user_connection.py
@@ -5,3 +5,13 @@ conn = sqlite3.connect("../../../library.db")
 
 # create a cursor/ pointer to the database
 c = conn.cursor()
+def Update_Column(M_name,Books_lended,Contact_No,DateOfJoining,MembersId ):
+    c.execute('''UPDATE Members SET Member name =?,Books_lended =?,Contact_No=?,DateOfJoining  = ? WHERE  MembersId = ?''',(M_name,Books_lended,Contact_No,DateOfJoining,MembersId))
+    conn.commit()
+
+#Fetch history of user from lending using user id
+
+def getByUserId(MemberId):
+    c.execute("SELECT * FROM Lending WHERE MemberId =?",(MemberId,))
+    history = c.fetchall()
+    return  tuple(history)
