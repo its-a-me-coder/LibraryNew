b4d3a199ab99ab98c0d78727c9780ab1eda4ea11
Sourik <dutta.sourik@gmail.com>
2022-11-29 11:29:15 +0530

    added 2 new functions one for User Table one for Lending table

--- LibraryMainApp/User/UsersSQL/user_connection.py
+++ LibraryMainApp/User/UsersSQL/user_connection.py
@@ -5,3 +5,13 @@ conn = sqlite3.connect("../../../library.db")
 
 # create a cursor/ pointer to the database
 c = conn.cursor()
+
+
+def delete_member(mem_id):
+    c.execute("DELETE FROM Members WHERE MembersId =?", (mem_id,))
+
+
+def history_book(book_id):
+    c.execute("SELECT * FROM Lending WHERE BookId=?", (book_id,))
+    return tuple(c.fetchall())
+
