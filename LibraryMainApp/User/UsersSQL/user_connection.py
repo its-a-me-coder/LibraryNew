1c852e8c2ad965cb634205c90ab91a98385f4c4a
Sourik <dutta.sourik@gmail.com>
2022-11-29 11:36:16 +0530

    added comments for the functions

--- LibraryMainApp/User/UsersSQL/user_connection.py
+++ LibraryMainApp/User/UsersSQL/user_connection.py
@@ -6,11 +6,11 @@ conn = sqlite3.connect("../../../library.db")
 # create a cursor/ pointer to the database
 c = conn.cursor()
 
-
+# Deletes a member using member id
 def delete_member(mem_id):
     c.execute("DELETE FROM Members WHERE MembersId =?", (mem_id,))
 
-
+# Gives all the records of a particular book
 def history_book(book_id):
     c.execute("SELECT * FROM Lending WHERE BookId=?", (book_id,))
     return tuple(c.fetchall())
