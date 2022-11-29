5bb9854aa5d712e700ba8806fcedf5e69000f120
sourogit85 <souradiptasaha8@gmail.com>
2022-11-29 14:23:24 +0530

    updated a func in bookconnection and added a function in user connection

--- LibraryMainApp/User/UsersSQL/user_connection.py
+++ LibraryMainApp/User/UsersSQL/user_connection.py
@@ -5,3 +5,18 @@ conn = sqlite3.connect("../../../library.db")
 
 # create a cursor/ pointer to the database
 c = conn.cursor()
+
+
+# addition of members
+
+def add_members(Name, booklended, cont):
+    c.execute('SELECT date("now")')
+    x = c.fetchall()
+    DateOfjoining = str(x[0])
+
+    c.execute("""INSERT INTO Members ('Member Name', Books_Lended,Contact_No, DateOfJoining) VALUES(?,?,?,?) """,
+              (Name, booklended, cont, DateOfjoining))
+
+    conn.commit()
+
+add_members("saha", 1, "38478374")
