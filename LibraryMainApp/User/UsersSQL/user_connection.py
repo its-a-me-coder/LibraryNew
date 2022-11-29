2427c6a171ac577b0cb171dc844a3f706c329565
Sourik <dutta.sourik@gmail.com>
2022-11-29 14:58:37 +0530

    library.db

--- LibraryMainApp/User/UsersSQL/user_connection.py
+++ LibraryMainApp/User/UsersSQL/user_connection.py
@@ -6,6 +6,19 @@ conn = sqlite3.connect("../../../library.db")
 # create a cursor/ pointer to the database
 c = conn.cursor()
 
+
+# addition of members
+
+def add_members(Name, booklended, cont):
+    c.execute('SELECT date("now")')
+    x = c.fetchall()
+    DateOfjoining = str(x[0])
+
+    c.execute("""INSERT INTO Members ('Member Name', Books_Lended,Contact_No, DateOfJoining) VALUES(?,?,?,?) """,
+              (Name, booklended, cont, DateOfjoining))
+
+    conn.commit()
+
 def Update_Contact(MembersId, Contact_No):
     c.execute('''UPDATE Members SET Contact_No = ?  WHERE  MembersId = ?''', (Contact_No, MembersId))
     conn.commit()
